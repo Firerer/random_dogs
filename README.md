bb84811eb55b13a8ff2e404484890798901cedf8
Di Liu <liudi12631@gmail.com>
2023-07-06 18:57:05 +1000

    refactor to testable code & add unit tests

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ## Project environment
 
 - project setup
-    ```sh
-    pnpm create vite random_dogs --template react-ts
-    ```
+
+  ```sh
+  pnpm create vite random_dogs --template react-ts
+  ```
+
+- running project: `pnpm run dev` or `pnpm run build && pnpm run preview`
+
+- testing project: `pnpm run test`
 
 - development environment: see `engine` in `package.json`
+
 - browser compatibility: see [vite's default browser compatibility](https://vitejs.dev/guide/build.html#browser-compatibility)
 
 ## Project decisions
@@ -14,7 +20,7 @@
 
   - reusing local packages, faster download speed
 
-- why vite?
+- why vite & vitest?
 
   - personal preference, easy of use & performance
 
@@ -25,4 +31,4 @@
 ## Things to improve
 
 1. robust error handing
-2. using browser's default video tag which has fixed aspect ratio instead of adjusting according to video size.
+2. small content size when display vertical videos
--- src/App.tsx
+++ src/App.tsx
@@ -6,56 +6,78 @@ type DogLinkResponse = {
   url: string;
 };
 
-async function fetchOnce(): Promise<{ link: string; type: string } | null> {
-  const response = await fetch("https://random.dog/woof.json");
-  const data = (await response.json()) as DogLinkResponse;
-  const matches = data.url.match(/(?!\.)\w+$/);
-  if (matches && matches.length == 1) {
-    switch (matches[0].toLowerCase()) {
-      case "jpg":
-      case "png":
-      case "gif":
-      case "mp4":
-        return { link: data.url, type: matches[0] };
-      default:
-        return null;
-    }
+const supportedMedias = ["jpg", "jpeg", "png", "gif", "mp4"] as const;
+type supportedMedia = (typeof supportedMedias)[number];
+
+function isSupportedLink(url: string): supportedMedia | null {
+  const matches = url.match(/(?!\.)\w+$/);
+  if (!matches || matches.length != 1) {
+    return null;
+  }
+
+  const match = matches[0].toLowerCase();
+  if (supportedMedias.includes(match as supportedMedia)) {
+    return match as supportedMedia;
   } else {
     return null;
   }
 }
 
-async function fetchRetry(retry = 3): ReturnType<typeof fetchOnce> {
+if (import.meta.vitest) {
+  const { describe, it, assert } = import.meta.vitest;
+
+  describe("isSupportedLink", () => {
+    it("should return the extension for supported media types", () => {
+      const testCases: { url: string; expected: supportedMedia }[] = [
+        { url: "http://example.com/image.jpg", expected: "jpg" },
+        { url: "http://example.com/video.mp4", expected: "mp4" },
+        { url: "http://example.com/image.gif", expected: "gif" },
+        { url: "http://example.com/image.png", expected: "png" },
+        { url: "http://example.com/image.jpeg", expected: "jpeg" },
+      ];
+
+      for (const { url, expected } of testCases) {
+        console.log("ans", isSupportedLink(url));
+        assert.strictEqual(isSupportedLink(url), expected);
+      }
+    });
+
+    it("should return null for unsupported media types", () => {
+      const testCases: string[] = [
+        "http://example.com/video.avi",
+        "http://example.com/",
+        "not-a-url",
+      ];
+
+      for (const url of testCases) {
+        assert.strictEqual(isSupportedLink(url), null);
+      }
+    });
+  });
+}
+
+async function fetchRetry(retry = 3): Promise<DogLinkResponse | null> {
   for (let i = 0; i < retry; i++) {
-    const result = await fetchOnce();
-    if (result) {
-      return result;
+    const response = await fetch("https://random.dog/woof.json");
+    const data = (await response.json()) as DogLinkResponse;
+    if (isSupportedLink(data.url)) {
+      return data;
     }
   }
   return null;
-}
-
-function Image({ url }: { url: string | null }) {
-  if (url) {
-    return <embed src={url} />;
-  } else {
-    return <p>loading...</p>;
-  }
 }
 
 function App() {
   const [urls, setUrls] = useState<(string | null)[]>(new Array(8).fill(null));
 
   const fetchData = async () => {
-    for (let i = 0; i < 8; i++) {
-      fetchRetry().then((result) => {
-        if (result) {
-          setUrls((v) =>
-            v.map((orignal, j) => (i == j ? result.link : orignal)),
-          );
-        }
-      });
-    }
+    const newUrls = await Promise.all(
+      urls.map(async (orignal) => {
+        const result = await fetchRetry();
+        return result ? result.url : orignal;
+      }),
+    );
+    return setUrls(newUrls);
   };
 
   useEffect(() => {
@@ -66,9 +88,15 @@ function App() {
     <>
       <button onClick={() => fetchData()}>Reload</button>
       <div className="waterfall-container">
-        {urls.map((url, i) => (
-          <Image url={url} key={i} />
-        ))}
+        {urls.map((url, i) =>
+          url ? (
+            <embed className="waterfall-item" key={i} src={url} />
+          ) : (
+            <p className="waterfall-item" key={i}>
+              loading...
+            </p>
+          ),
+        )}
       </div>
     </>
   );
